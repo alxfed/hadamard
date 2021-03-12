752370595cf1a6649962e30212905e80ed900514
alxfed <alxfed@gmail.com>
2021-03-12 17:40:41 -0600

    repeat data

--- repeat_data.py
+++ repeat_data.py
@@ -17,13 +17,20 @@ def hadamard(n):
     return H
 
 
+def hadamard2(n):
+    H = np.array([[1]], dtype=int)
+    for i in range(0, n):
+        H = np.block([[H, H], [H, -H]])
+    return H
+
+
 def main():
     data = [1, 2, 3, 4]
     # data_m = np.array([data,] * len(data))
     # more literate
     data_a = np.array(data)
     data_m = np.tile(data_a, (4, 1))
-    h4 = hadamard(2)
+    h4 = hadamard2(2)
     trans = data_m * h4
     print(trans)
     return
