6d4ebfbb6661b1778afa514dc8786b010c312496
alxfed <alxfed@gmail.com>
2021-03-12 16:37:50 -0600

    repeat data

--- repeat_data.py
+++ repeat_data.py
@@ -7,7 +7,10 @@ from scipy.linalg import hadamard
 
 def main():
     data = [1, 2, 3, 4]
-    data_m = np.array([data,] * len(data))
+    # data_m = np.array([data,] * len(data))
+    # more literate
+    data_a = np.array(data)
+    data_m = np.tile(data_a, (4, 1))
     h4 = hadamard(4, dtype=int)
     trans = data_m * h4
     print(trans)
